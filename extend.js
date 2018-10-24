45e462cbe4b8959498c2028f71ad611a53ab20e5
Mike MacCana <mike@certsimple.com>
2018-10-24 10:39:44 +0100

    Prettier

--- extend.js
+++ extend.js
@@ -1,4 +1,3 @@
-
 /* Highly dumbed down version of https://github.com/unclechu/node-deep-extend */
 
 /**
@@ -11,36 +10,36 @@
  *   deepExtend({}, yourObj_1, [yourObj_N]);
  */
 module.exports = function deepExtend(/*obj_1, [obj_2], [obj_N]*/) {
-	if (arguments.length < 1 || typeof arguments[0] !== 'object') {
-		return false;
+	if (arguments.length < 1 || typeof arguments[0] !== "object") {
+		return false
 	}
 
 	if (arguments.length < 2) {
-		return arguments[0];
+		return arguments[0]
 	}
 
-	var target = arguments[0];
+	var target = arguments[0]
 
 	for (var i = 1; i < arguments.length; i++) {
-		var obj = arguments[i];
+		var obj = arguments[i]
 
 		for (var key in obj) {
-			var src = target[key];
-			var val = obj[key];
+			var src = target[key]
+			var val = obj[key]
 
-			if (typeof val !== 'object' || val === null) {
-				target[key] = val;
+			if (typeof val !== "object" || val === null) {
+				target[key] = val
 
 				// just clone arrays (and recursive clone objects inside)
-			} else if (typeof src !== 'object' || src === null) {
-				target[key] = deepExtend({}, val);
+			} else if (typeof src !== "object" || src === null) {
+				target[key] = deepExtend({}, val)
 
 				// source value and new value is objects both, extending...
 			} else {
-				target[key] = deepExtend(src, val);
+				target[key] = deepExtend(src, val)
 			}
 		}
 	}
 
-	return target;
-};
+	return target
+}
--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-var UserAgentParser = require('ua-parser-js');
-var languageMessages = require('./languages.json');
-var deepExtend = require('./extend');
+var UserAgentParser = require("ua-parser-js")
+var languageMessages = require("./languages.json")
+var deepExtend = require("./extend")
 
 var DEFAULTS = {
-	'Chrome': 57, // Includes Chrome for mobile devices
-	'Edge': 39,
-	'Safari': 10,
-	'Mobile Safari': 10,
-	'Opera': 50,
-	'Firefox': 50,
-	'Vivaldi': 1,
-	'IE': false
-};
+	Chrome: 57, // Includes Chrome for mobile devices
+	Edge: 39,
+	Safari: 10,
+	"Mobile Safari": 10,
+	Opera: 50,
+	Firefox: 50,
+	Vivaldi: 1,
+	IE: false
+}
 
 var EDGEHTML_VS_EDGE_VERSIONS = {
 	12: 0.1,
@@ -20,219 +20,233 @@ var EDGEHTML_VS_EDGE_VERSIONS = {
 	15: 39,
 	16: 41,
 	17: 42
-};
+}
 
 var COLORS = {
-	salmon: '#f25648',
-	white: 'white'
+	salmon: "#f25648",
+	white: "white"
 }
 
 var updateDefaults = function(defaults, updatedValues) {
 	for (var key in updatedValues) {
 		if (defaults.hasOwnProperty(key)) {
-			defaults[key] = updatedValues[key];
+			defaults[key] = updatedValues[key]
 		}
 	}
 
-	return defaults;
-};
+	return defaults
+}
 
-module.exports = function (options) {
-
-	var main = function () {
-
+module.exports = function(options) {
+	var main = function() {
 		// Despite the docs, UA needs to be provided to constructor explicitly:
 		// https://github.com/faisalman/ua-parser-js/issues/90
-		var parsedUserAgent = new UserAgentParser(window.navigator.userAgent).getResult();
+		var parsedUserAgent = new UserAgentParser(window.navigator.userAgent).getResult()
 
 		// Variable definition (before ajax)
-		var outdatedUI = document.getElementById('outdated');
+		var outdatedUI = document.getElementById("outdated")
 
-		options = options || {};
+		options = options || {}
 
-		var browserLocale = window.navigator.language || window.navigator.userLanguage; // Everyone else, IE
+		var browserLocale = window.navigator.language || window.navigator.userLanguage // Everyone else, IE
 
 		// Set default options
-		var browserSupport = options.browserSupport ? updateDefaults(DEFAULTS, options.browserSupport) : DEFAULTS;
+		var browserSupport = options.browserSupport ? updateDefaults(DEFAULTS, options.browserSupport) : DEFAULTS
 		// CSS property to check for. You may also like 'borderSpacing', 'boxShadow', 'transform', 'borderImage';
-		var requiredCssProperty = options.requiredCssProperty || false;
-		var backgroundColor = options.backgroundColor || COLORS.salmon;
-		var textColor = options.textColor || COLORS.white;
-		var fullscreen = options.fullscreen || false;
-		var language = options.language || browserLocale.slice(0, 2); // Language code
+		var requiredCssProperty = options.requiredCssProperty || false
+		var backgroundColor = options.backgroundColor || COLORS.salmon
+		var textColor = options.textColor || COLORS.white
+		var fullscreen = options.fullscreen || false
+		var language = options.language || browserLocale.slice(0, 2) // Language code
 
-		var updateSource = 'web'; // Other possible values are 'googlePlay' or 'appStore'. Determines where we tell users to go for upgrades.
+		var updateSource = "web" // Other possible values are 'googlePlay' or 'appStore'. Determines where we tell users to go for upgrades.
 
 		// Chrome mobile is still Chrome (unlike Safari which is 'Mobile Safari')
-		var isAndroid = parsedUserAgent.os.name === 'Android';
-		if ( isAndroid ) {
-			updateSource = 'googlePlay';
+		var isAndroid = parsedUserAgent.os.name === "Android"
+		if (isAndroid) {
+			updateSource = "googlePlay"
 		}
 
-		var isAndroidButNotChrome;
+		var isAndroidButNotChrome
 		if (options.requireChromeOnAndroid) {
-			isAndroidButNotChrome = (isAndroid) && (parsedUserAgent.browser.name !== 'Chrome');
+			isAndroidButNotChrome = isAndroid && parsedUserAgent.browser.name !== "Chrome"
 		}
 
-		if (parsedUserAgent.os.name === 'iOS') {
-			updateSource = 'appStore';
+		if (parsedUserAgent.os.name === "iOS") {
+			updateSource = "appStore"
 		}
 
-		var done = true;
+		var done = true
 
-		var changeOpacity = function (opacityValue) {
-			outdatedUI.style.opacity = opacityValue / 100;
-			outdatedUI.style.filter = 'alpha(opacity=' + opacityValue + ')';
-		};
+		var changeOpacity = function(opacityValue) {
+			outdatedUI.style.opacity = opacityValue / 100
+			outdatedUI.style.filter = "alpha(opacity=" + opacityValue + ")"
+		}
 
-		var fadeIn = function (opacityValue) {
-			changeOpacity(opacityValue);
+		var fadeIn = function(opacityValue) {
+			changeOpacity(opacityValue)
 			if (opacityValue === 1) {
-				outdatedUI.style.display = 'table';
+				outdatedUI.style.display = "table"
 			}
 			if (opacityValue === 100) {
-				done = true;
+				done = true
 			}
-		};
+		}
 
-		var isBrowserOutOfDate = function () {
-			var browserName = parsedUserAgent.browser.name;
-			var browserMajorVersion = parsedUserAgent.browser.major;
-			if ( browserName === 'Edge' ) {
+		var isBrowserOutOfDate = function() {
+			var browserName = parsedUserAgent.browser.name
+			var browserMajorVersion = parsedUserAgent.browser.major
+			if (browserName === "Edge") {
 				browserMajorVersion = EDGEHTML_VS_EDGE_VERSIONS[browserMajorVersion]
 			}
-			var isOutOfDate = false;
-			if ( ! browserSupport[browserName] ) {
-				if ( ! options.isUnknownBrowserOK ) {
-					isOutOfDate = true;
+			var isOutOfDate = false
+			if (!browserSupport[browserName]) {
+				if (!options.isUnknownBrowserOK) {
+					isOutOfDate = true
 				}
 			} else if (browserMajorVersion < browserSupport[browserName]) {
-				isOutOfDate = true;
+				isOutOfDate = true
 			}
-			return isOutOfDate;
-		};
+			return isOutOfDate
+		}
 
 		// Returns true if a browser supports a css3 property
-		var isPropertySupported = function (property) {
-			if ( ! property ) {
-				return true;
+		var isPropertySupported = function(property) {
+			if (!property) {
+				return true
 			}
-			var div = document.createElement('div');
-			var vendorPrefixes = ['khtml', 'ms', 'o', 'moz', 'webkit'];
-			var count = vendorPrefixes.length;
+			var div = document.createElement("div")
+			var vendorPrefixes = ["khtml", "ms", "o", "moz", "webkit"]
+			var count = vendorPrefixes.length
 
-			if ( div.style.hasOwnProperty(property) ) {
-				return true;
+			if (div.style.hasOwnProperty(property)) {
+				return true
 			}
 
-			property = property.replace(/^[a-z]/, function (val) {
-				return val.toUpperCase();
-			});
+			property = property.replace(/^[a-z]/, function(val) {
+				return val.toUpperCase()
+			})
 
 			while (count--) {
 				var prefixedProperty = vendorPrefixes[count] + property
-				if ( div.style.hasOwnProperty(prefixedProperty) ) {
-					return true;
+				if (div.style.hasOwnProperty(prefixedProperty)) {
+					return true
 				}
 			}
-			return false;
-		};
+			return false
+		}
 
 		var makeFadeInFunction = function(opacityValue) {
-			return function () {
-				fadeIn(opacityValue);
-			};
-		};
+			return function() {
+				fadeIn(opacityValue)
+			}
+		}
 
 		// Style element explicitly - TODO: investigate and delete if not needed
-		var startStylesAndEvents = function () {
-			var buttonClose = document.getElementById('buttonCloseUpdateBrowser');
-			var buttonUpdate = document.getElementById('buttonUpdateBrowser');
+		var startStylesAndEvents = function() {
+			var buttonClose = document.getElementById("buttonCloseUpdateBrowser")
+			var buttonUpdate = document.getElementById("buttonUpdateBrowser")
 
 			//check settings attributes
-			outdatedUI.style.backgroundColor = backgroundColor;
+			outdatedUI.style.backgroundColor = backgroundColor
 			//way too hard to put !important on IE6
-			outdatedUI.style.color = textColor;
-			outdatedUI.children[0].children[0].style.color = textColor;
-			outdatedUI.children[0].children[1].style.color = textColor;
+			outdatedUI.style.color = textColor
+			outdatedUI.children[0].children[0].style.color = textColor
+			outdatedUI.children[0].children[1].style.color = textColor
 
 			// Update button is desktop only
 			if (buttonUpdate) {
-				buttonUpdate.style.color = textColor;
+				buttonUpdate.style.color = textColor
 				if (buttonUpdate.style.borderColor) {
-					buttonUpdate.style.borderColor = textColor;
+					buttonUpdate.style.borderColor = textColor
 				}
 
 				// Override the update button color to match the background color
-				buttonUpdate.onmouseover = function () {
-					this.style.color = backgroundColor;
-					this.style.backgroundColor = textColor;
-				};
+				buttonUpdate.onmouseover = function() {
+					this.style.color = backgroundColor
+					this.style.backgroundColor = textColor
+				}
 
-				buttonUpdate.onmouseout = function () {
-					this.style.color = textColor;
-					this.style.backgroundColor = backgroundColor;
-				};
-			}
-
-			buttonClose.style.color = textColor;
-
-			buttonClose.onmousedown = function () {
-				outdatedUI.style.display = 'none';
-				return false;
-			};
-		};
-
-		var getmessage = function (lang) {
-			var defaultMessages = languageMessages[lang] || languageMessages.en;
-			var customMessages = options.messages && options.messages[lang];
-			var messages = deepExtend({}, defaultMessages, customMessages);
-
-			var updateMessages = {
-				'web': '<p>' + messages.update.web + '<a id="buttonUpdateBrowser" rel="nofollow" href="' + messages.url + '">' + messages.callToAction + '</a></p>',
-				'googlePlay': '<p>' + messages.update.googlePlay +
-				'<a id="buttonUpdateBrowser" rel="nofollow" href="https://play.google.com/store/apps/details?id=com.android.chrome">' + messages.callToAction + '</a></p>',
-				'appStore': '<p>' + messages.update[updateSource] + '</p>'
-			};
-
-			var updateMessage = updateMessages[updateSource];
-
-			return '<div class="vertical-center"><h6>' + messages.outOfDate + '</h6>' + updateMessage +
-				'<p class="last"><a href="#" id="buttonCloseUpdateBrowser" title="' + messages.close + '">&times;</a></p></div>';
-		};
-
-		// Check if browser is supported
-		if ( isBrowserOutOfDate() || ! isPropertySupported(requiredCssProperty) || isAndroidButNotChrome ) {
-
-			// This is an outdated browser
-			if (done && outdatedUI.style.opacity !== '1') {
-				done = false;
-
-				for (var opacity = 1; opacity <= 100; opacity++) {
-					setTimeout(makeFadeInFunction(opacity), opacity * 8);
+				buttonUpdate.onmouseout = function() {
+					this.style.color = textColor
+					this.style.backgroundColor = backgroundColor
 				}
 			}
 
-			var insertContentHere = document.getElementById('outdated');
-			if(fullscreen){
-				insertContentHere.classList.add("fullscreen");
+			buttonClose.style.color = textColor
+
+			buttonClose.onmousedown = function() {
+				outdatedUI.style.display = "none"
+				return false
 			}
-			insertContentHere.innerHTML = getmessage(language);
-			startStylesAndEvents();
 		}
-	};
+
+		var getmessage = function(lang) {
+			var defaultMessages = languageMessages[lang] || languageMessages.en
+			var customMessages = options.messages && options.messages[lang]
+			var messages = deepExtend({}, defaultMessages, customMessages)
+
+			var updateMessages = {
+				web:
+					"<p>" +
+					messages.update.web +
+					'<a id="buttonUpdateBrowser" rel="nofollow" href="' +
+					messages.url +
+					'">' +
+					messages.callToAction +
+					"</a></p>",
+				googlePlay:
+					"<p>" +
+					messages.update.googlePlay +
+					'<a id="buttonUpdateBrowser" rel="nofollow" href="https://play.google.com/store/apps/details?id=com.android.chrome">' +
+					messages.callToAction +
+					"</a></p>",
+				appStore: "<p>" + messages.update[updateSource] + "</p>"
+			}
+
+			var updateMessage = updateMessages[updateSource]
+
+			return (
+				'<div class="vertical-center"><h6>' +
+				messages.outOfDate +
+				"</h6>" +
+				updateMessage +
+				'<p class="last"><a href="#" id="buttonCloseUpdateBrowser" title="' +
+				messages.close +
+				'">&times;</a></p></div>'
+			)
+		}
+
+		// Check if browser is supported
+		if (isBrowserOutOfDate() || !isPropertySupported(requiredCssProperty) || isAndroidButNotChrome) {
+			// This is an outdated browser
+			if (done && outdatedUI.style.opacity !== "1") {
+				done = false
+
+				for (var opacity = 1; opacity <= 100; opacity++) {
+					setTimeout(makeFadeInFunction(opacity), opacity * 8)
+				}
+			}
+
+			var insertContentHere = document.getElementById("outdated")
+			if (fullscreen) {
+				insertContentHere.classList.add("fullscreen")
+			}
+			insertContentHere.innerHTML = getmessage(language)
+			startStylesAndEvents()
+		}
+	}
 
 	// Load main when DOM ready.
-	var oldOnload = window.onload;
-	if (typeof window.onload !== 'function') {
-		window.onload = main;
-	}
-	else {
-		window.onload = function () {
-			if ( oldOnload ) {
-				oldOnload();
+	var oldOnload = window.onload
+	if (typeof window.onload !== "function") {
+		window.onload = main
+	} else {
+		window.onload = function() {
+			if (oldOnload) {
+				oldOnload()
 			}
-			main();
-		};
+			main()
+		}
 	}
-};
+}
